48dbf9723abba26ecdd815fcf4ca74b0af2aa4a9
Gongkangjia <525242664@qq.com>
2020-04-01 10:19:37 +0800

    add diy

--- diy.sh
+++ diy.sh
@@ -6,4 +6,28 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+# 更改默认主题为Argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' package/feeds/luci/luci/Makefile
+git clone https://github.com/jerrykuku/luci-theme-argon.git -b 18.06 package/mine/luci-theme-argon_new
+ed -i 's/luci-theme-bootstrap/luci-theme-argon_new/g' package/feeds/luci/luci/Makefile
+
+# 复杂的AdGuardHome的openwrt的luci界面
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/mine/luci-app-adguardhome
+
+# DiskMan for LuCI (WIP)
+#git clone https://github.com/lisaac/luci-app-diskman.git package/mine/luci-app-diskman
+#mkdir -p package/mine/parted && cp -i package/mine/luci-app-diskman/Parted.Makefile package/mine/parted/Makefile
+
+# KPR plus+
+git clone https://github.com/project-openwrt/luci-app-koolproxyR.git package/mine/luci-app-koolproxyR
+
+# Server酱
+git clone https://github.com/tty228/luci-app-serverchan.git package/mine/luci-app-serverchan
+
+# FileBrowser
+#git clone https://github.com/project-openwrt/FileBrowser.git package/mine/FileBrowser
+
+# 网易云音乐
+git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic.git package/mine/luci-app-unblockneteasemusic
+
